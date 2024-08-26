fcf0e584fb20f504fe0b2579e490471e81f23e05
RexndleZero <raihanaditya1506@gmail.com>
2024-08-26 10:45:35 +0700

    update cors

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,7 @@ package config
 import "net/http"
 
 var Origins = []string{
-	"https://ruteangkot.github.io/",
+	"https://libro-electronico.github.io",
 	
 }
 
@@ -26,14 +26,14 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
 		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
-		w.Header().Set("Access-Control-Allow-Origin", "https://ruteangkot.github.io")
+		w.Header().Set("Access-Control-Allow-Origin", "https://libro-electronico.github.io")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
 		return true
 	}
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", "https://ruteangkot.github.io")
+	w.Header().Set("Access-Control-Allow-Origin", "https://libro-electronico.github.io")
 	w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
 	return false
 }
